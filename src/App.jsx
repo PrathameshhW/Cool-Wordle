aa7ed2fed8af562f958cc1799d14fd4033f1b8b6
Prathamesh Jadhav <prathameshj0407@gmail.com>
2023-02-10 10:46:35 +0530

    add header compnonent says Wordplay

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./app.css";
+import Header from "./components/header";
+function App() {
+  return (
+    <>
+      <div className="app-conatiner">
+        <Header />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./header.css";
+
+const header = () => {
+  return (
+    <>
+      <div className="header-container">
+        <div className="header-text">Wordplay</div>
+      </div>
+    </>
+  );
+};
+
+export default header;
